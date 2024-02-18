e641437455420279e9ee1c30b4532b335bf5aefc
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-02-17 19:43:15 -0800

    Created working 16x16 grid with borders

--- script.js
+++ script.js
@@ -12,8 +12,17 @@ each grid row (height = HEIGHT/16, width = HEIGHT)
 each grid within the row (height = HEIGHT/16, width = HEIGHT/16)
 */
 
+
+// Easy revamp in the future for the button, change 16 to input value that
+// User inputs and when the screen refreshes, preload it with 16
 for (let gridRow = 0; gridRow < 16; gridRow++) {
     let row = document.createElement('div');
-    row.style.cssText = `height: ${HEIGHT/16}px; width: ${HEIGHT}px; border: 1px solid black;`;
+    row.setAttribute('class', 'grid-row');
+    row.style.cssText = `height: ${HEIGHT/16}px; width: ${HEIGHT}px;`;
     grid.appendChild(row);
+    for (let rowItem = 0; rowItem < 16; rowItem++) {
+        let item = document.createElement('div');
+        item.style.cssText = `height: ${HEIGHT/16}px; width: ${HEIGHT/16}px; border: 1px solid red;`;
+        row.appendChild(item);
+    }
 }
